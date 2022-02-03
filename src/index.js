7abaad0eb92574b7195f6a219c99d913aad0cccc
Jolanta Kazmierczak <jolanta.kazmierczak99@gmail.com>
2022-02-03 12:34:36 +0100

    redux middleware

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import {Provider} from 'react-redux';
+import {applyMiddleware, createStore} from 'redux';
+import {createLogger} from "redux-logger";
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import App from './containers/App';
 import {searchRobots} from "./reducers";
 
-const store = createStore(searchRobots)
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger))
 
 ReactDOM.render(
-  <React.StrictMode>
-      <div>
-          <Provider store={store}>
-              <App/>
-          </Provider>
-      </div>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <div>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </div>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 
