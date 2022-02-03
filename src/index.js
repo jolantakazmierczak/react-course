e7d2f8e352e0f5187e91aa026042169cf0e6b49f
Jolanta Kazmierczak <jolanta.kazmierczak99@gmail.com>
2022-02-03 11:58:54 +0100

    store and provider

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider, connect } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import App from './containers/App';
+import {searchRobots} from "./reducers";
+
+const store = createStore(searchRobots)
 
 ReactDOM.render(
   <React.StrictMode>
       <div>
-          <App />
+          <Provider store={store}>
+              <App/>
+          </Provider>
       </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
